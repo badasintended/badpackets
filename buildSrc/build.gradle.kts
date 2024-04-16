f4d9ff33f8b5d483e0ed0a4a2f6f1e7dc25caded
deirn <deirn@bai.lol>
2024-04-16 18:05:59 +0700

    pin gradle plugin version, update to mod-publish-plugin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -6,17 +6,15 @@ plugins {
 repositories {
     mavenCentral()
     gradlePluginPortal()
-    maven("https://jitpack.io")
 
     // https://github.com/MinecraftForge/ForgeGradle/issues/919
     maven("https://maven.minecraftforge.net")
 }
 
 dependencies {
-    implementation("com.modrinth.minotaur:Minotaur:2.2.1")
-    implementation("com.github.deirn:CurseForgeGradle:c693018f92")
+    implementation("me.modmuss50:mod-publish-plugin:0.5.1")
 
     // https://github.com/MinecraftForge/ForgeGradle/issues/919
     // The issue still happens because FART still uses outdated srgutils version
-    implementation("net.minecraftforge:srgutils:0.5.3")
+    implementation("net.minecraftforge:srgutils:0.5.10")
 }
