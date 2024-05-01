95024f566ab5b9b755528074d27275c36e8b885a
deirn <deirn@bai.lol>
2024-05-01 17:37:59 +0700

    fix build

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -6,8 +6,15 @@ plugins {
 repositories {
     mavenCentral()
     gradlePluginPortal()
+
+    // https://github.com/MinecraftForge/ForgeGradle/issues/919
+    maven("https://maven.minecraftforge.net")
 }
 
 dependencies {
     implementation("me.modmuss50:mod-publish-plugin:0.5.1")
+
+    // https://github.com/MinecraftForge/ForgeGradle/issues/919
+    // The issue still happens because FART still uses outdated srgutils version
+    implementation("net.minecraftforge:srgutils:0.5.10")
 }
